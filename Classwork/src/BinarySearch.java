a84c580ca701bcd0c183fdb6e7f512ae4da2d93c
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-11-15 12:40:28 -0500

    11/15

--- Classwork/src/BinarySearch.java
+++ Classwork/src/BinarySearch.java
@@ -3,13 +3,19 @@ public class BinarySearch
 {
 	public static void main (String[] args)
 	{
-		int[] test1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		int[] test2 = {0, 4, 5, 6, 12, 15, 17, 23, 29};
+		int[] test1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		int[] test2 = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 		
-		System.out.println(binarySearch(test1, 3, 0, test1.length)); // 2
-		System.out.println(binarySearch(test2, 23, 0, test2.length)); // 7
-		System.out.println(binarySearch(test1, 1, 0, test1.length)); // 0
-		System.out.println(binarySearch(test2, 25, 0, test2.length)); // -1
+		for (int i = -1; i < test1.length; i++)
+		{
+			System.out.println(binarySearch(test1, i, 0, test1.length - 1));
+		}
+		System.out.println();
+		for (int i = -1; i < test2.length; i++)
+		{
+			System.out.println(binarySearch(test2, i, 0, test2.length - 1));
+		}
+		System.out.println();
 	}
 	
 	public static int binarySearch(int[] nums, int query, int first, int last)
